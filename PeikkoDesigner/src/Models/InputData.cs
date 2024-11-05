0ddeda9361c8d3e376e788e85feda1be1b3b2083
tieud1 <duykha.tieu@aalto.fi>
2024-11-06 00:44:35 +0200

    facilitate Layer and InputData classes

--- PeikkoDesigner/src/Models/InputData.cs
+++ PeikkoDesigner/src/Models/InputData.cs
@@ -1,51 +1,59 @@
-public class Layer
-{
-	public string Name { get; set; }
-	public int X { get; set; }
-	public int Y { get; set; }
-	public int Width { get; set; }
-	public int Height { get; set; }
-	public int Thickness { get; set; }
-
-	public Layer(string name, int x, int y, int width, int height, int thickness)
-    {
-        Name = name;
-        X = x;
-        Y = y;
-        Width = width;
-        Height = height;
-        Thickness = thickness;
-    }
-	public void Print()
-    {
-        Console.WriteLine($"{Name}:");
-        Console.WriteLine($"  X: {X}");
-        Console.WriteLine($"  Y: {Y}");
-        Console.WriteLine($"  Width: {Width}");
-        Console.WriteLine($"  Height: {Height}");
-        Console.WriteLine($"  Thickness: {Thickness}");
-    } 
-}
 
 public class InputData
 {
-    public Layer ExternalLayer { get; set; }
-    public Layer InternalLayer { get; set; }
-    public Layer InsulatedLayer { get; set; }
-    public Layer Hole { get; set; }
+	//---------------------------------------------
+	// Properties
+	//---------------------------------------------
+	public Layer ExternalLayer { get; set; }
+	public Layer InternalLayer { get; set; }
+	public int InsulatedLayerThickness { get; set; }
+	public Hole Hole { get; set; }
+	public Layer InsulatedLayer { get; private set; }
+	
+	//---------------------------------------------
+	// Constructor 
+	//---------------------------------------------
+	public InputData(Layer externalLayer, Layer internalLayer, int insulatedLayerThickness, Hole hole)
+	{
+		ExternalLayer = externalLayer;
+		InternalLayer = internalLayer;
+		InsulatedLayer = new Layer(
+			"InsulatedLayer",
+			internalLayer.X,
+			internalLayer.Y,
+			internalLayer.Width,
+			internalLayer.Height,
+			insulatedLayerThickness);
+		Hole = hole;
+		hole.SetThickness(ExternalLayer, InternalLayer, InsulatedLayer);
+	}
+	//---------------------------------------------
+	// Methods 
+	//---------------------------------------------
+	public bool	Check()
+	{
+		if (ValidateCenterGravity(ExternalLayer, InternalLayer))
+			return (true);
+		return (false);
+	}
 
-    public InputData(Layer externalLayer, Layer internalLayer, Layer insulatedLayer, Layer hole)
-    {
-        ExternalLayer = externalLayer;
-        InternalLayer = internalLayer;
-        InsulatedLayer = insulatedLayer;
-        Hole = hole;
-    }
 	public void PrintLayers()
-    {
+	{
 		ExternalLayer.Print();
 		InternalLayer.Print();
 		InsulatedLayer.Print();
 		Hole.Print();
-    }
+	}
+
+	//---------------------------------------------
+	// Helper functions 
+	//---------------------------------------------s
+	private static bool ValidateCenterGravity(Layer eLayer, Layer iLayer)
+	{
+		int externalCenterX = eLayer.X + (eLayer.Width / 2);
+		int externalCenterY = eLayer.Y + (eLayer.Height / 2);
+		return (iLayer.X <= externalCenterX && externalCenterX <= iLayer.X + iLayer.Width)
+			&& (iLayer.Y <= externalCenterY && externalCenterY <= iLayer.Y + iLayer.Height);
+	}
+
 }
---
+++ PeikkoDesigner/src/Models/Layer.cs
@@ -0,0 +1,94 @@
+using System.Text.Json.Serialization;
+
+public class Layer
+{
+	//---------------------------------------------
+	// Properties
+	//---------------------------------------------
+	public string Name { get; set; }
+	public int X { get; set; }
+	public int Y { get; set; }
+	public int Width { get; set; }
+	public int Height { get; set; }
+	public int Thickness { get; set; }
+
+	//---------------------------------------------
+	// Constructor 
+	//---------------------------------------------
+	public Layer(string name, int x, int y, int width, int height, int thickness)
+	{
+		Name = name;
+		X = x;
+		Y = y;
+		Width = width;
+		Height = height;
+		Thickness = thickness;
+	}
+	
+	//---------------------------------------------
+	// Methods 
+	//---------------------------------------------
+	public virtual void Print()
+	{
+		Console.WriteLine("------------------------");
+		Console.WriteLine($"{Name}:");
+		Console.WriteLine("------------------------");
+		Console.WriteLine($"X: {X}");
+		Console.WriteLine($"Y: {Y}");
+		Console.WriteLine($"Width: {Width}");
+		Console.WriteLine($"Height: {Height}");
+		Console.WriteLine($"Thickness: {Thickness}");
+	} 
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum HolePosition
+{
+    Internal, External, Both
+}
+
+public class Hole : Layer
+{
+	public HolePosition Position { get; set; }
+
+	public Hole(string name, int x, int y, int width, int height, HolePosition position)
+		: base(name, x, y, width, height, 0)
+	{
+		Position = position;
+	}
+
+	public  int SetThickness(
+		Layer externalLayer,
+		Layer internalLayer,
+		Layer insulatedLayer)
+	{
+		if (internalLayer == null || externalLayer == null)
+			return (0);
+		switch (Position)
+		{
+			case HolePosition.Internal:
+				Thickness = internalLayer.Thickness;
+				return (Thickness);
+			case HolePosition.External:
+				Thickness = externalLayer.Thickness;
+				return (Thickness);
+			case HolePosition.Both:
+				int thickness = 0;
+				if (internalLayer != null)
+					thickness += internalLayer.Thickness;
+				if (insulatedLayer != null)
+					thickness += insulatedLayer.Thickness;
+				if (externalLayer != null)
+					thickness += externalLayer.Thickness;
+				Thickness = thickness;
+				return (Thickness);
+			default:
+				return (0);
+		}
+	}
+	public override void Print()
+	{
+		base.Print();
+		Console.WriteLine($"Position: {Position}");
+	}
+}
